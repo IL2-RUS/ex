9aaf2007c62d6e161446f8acb21471df741cf150
IL2-RUS <gavrilov.il2@gmail.com>
2016-07-20 10:32:05 +0000

    example

---
+++ keras_first_network.py
@@ -0,0 +1,85 @@
+from keras.utils.data_utils import get_file
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense, Activation
+import random
+import sys
+
+path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open(path).read().lower()
+print('corpus len: ', len(text))
+
+chars = sorted(list(set(text)))
+print('total chars: ', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+maxlen = 40 
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print ('nb sequences: ', len(sentences))
+
+print ('Vectorization...')
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[char]] = 1
+
+print ('Build model...')
+model = Sequential()
+model.add(LSTM(512, unroll=True, return_sequences=True, input_shape=(maxlen, len(chars))))
+model.add(LSTM(512, unroll=True, return_sequences=False)) 
+model.add(Dropout(0.2)) 
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop') 
+
+def sample(a, temperature=1.0):
+
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+
+for iteration in range(1, 60):
+    print()
+    print('-' * 50)
+    print('Iteration', iteration)
+
+    model.fit(X, y, batch_size=128, nb_epoch=1)
+    
+    start_index = random.randint(0, len(text) - maxlen - 1)
+
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print('----- diversity: ', diversity)
+        
+        generated = ''
+        sentence = text[start_index : start_index + maxlen]
+        generated += sentence
+        print ('----- generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+        
+        for i in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1
+            preds = model.predict(x, verbose=0)[0]
+
+
+            next_index = sample(preds, diversity) 
+            next_char = indices_char[next_index]
+            
+            generated += next_char 
+            sentence = sentence[1:] + next_char 
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
